a0f9169ca20a787c66628c50eadbb5cc13590ba8
ThunderBrown <marcos.ellys@gmail.com>
2014-09-21 00:38:19 -0300

    Atividades finalizadas por hoje

--- app/controllers/UnidadesController.php
+++ app/controllers/UnidadesController.php
@@ -1,71 +1,71 @@
 <?php
 
-	class UnidadesController extends BaseController{
+class UnidadesController extends BaseController{
 
-		public function index()
-		{
+	public function index()
+	{
 
-			$query = Unidade::paginate(5);
+		$query = Unidade::paginate(5);
 
-			$vars = array('unidades' => $query);
+		$vars = array('unidades' => $query);
 
-			$this->layout->content = View::make('unidades.index',$vars);
+		$this->layout->content = View::make('unidades.index',$vars);
 
+	}
+
+	public function create()
+	{
+		$this->layout->content = View::make('unidades.create');
+	}
+
+	public function store()
+	{
+
+		$unidade = new Unidade();
+
+		$unidade->nome = Input::get('nome');
+		$unidade->rua = Input::get('rua');
+		$unidade->bairro = Input::get('bairro');		
+		$unidade->numero = Input::get('numero');
+		$unidade->complemento = Input::get('complemento');
+		$unidade->tipo = Input::get('tipo');
+
+
+		$rules = array(
+			'nome' => 'required|alpha|between:5,40',
+			'rua' => 'required|between:5,35',
+			'bairro' => 'required|between:5,30',
+			'numero' => 'required|numeric',
+			'tipo' => 'required'
+			);
+
+		$validator = Validator::make(Input::all(), $rules);
+
+		if ($validator->passes()) {
+
+			$unidade->save();
+
+			return Redirect::action('UnidadesController@index');
+
+		} else {
+
+			return Redirect::to('unidades/create')->withInput()->withErrors($validator);
 		}
 
-		public function create()
-		{
-			$this->layout->content = View::make('unidades.create');
-		}
+	}
 
-		public function store()
-		{
+	public function show($id)
+	{
 
-			$unidade = new Unidade();
-
-			$unidade->nome = Input::get('nome');
-			$unidade->rua = Input::get('rua');
-			$unidade->bairro = Input::get('bairro');		
-			$unidade->numero = Input::get('numero');
-			$unidade->complemento = Input::get('complemento');
-			$unidade->tipo = Input::get('tipo');
-
-
-			$rules = array(
-				'nome' => 'required|alpha|between:5,40',
-				'rua' => 'required|between:5,35',
-				'bairro' => 'required|between:5,30',
-				'numero' => 'required|numeric',
-				'tipo' => 'required'
-				);
-
-			$validator = Validator::make(Input::all(), $rules);
-
-			if ($validator->passes()) {
-
-				$unidade->save();
-
-				return Redirect::action('UnidadesController@index');
-
-			} else {
-
-				return Redirect::to('unidades/create')->withInput()->withErrors($validator);
-			}
-
-		}
-
-		public function show($id)
-		{
-			
 		$result = Unidade::find($id);
 
 		$vars = array('unidade' => $result);
 
 		$this->layout->content = View::make('unidades.show',$vars);
 
-		}
+	}
 
-		public function edit($id)
+	public function edit($id)
 	{
 		$unidade = Unidade::find($id);
 
@@ -87,25 +87,25 @@
 		$unidade->tipo = Input::get('tipo');
 
 		$rules = array(
-				'nome' => 'required|alpha|between:5,40',
-				'rua' => 'required|between:5,35',
-				'bairro' => 'required|between:5,30',
-				'numero' => 'required|numeric',
-				'tipo' => 'required'
-				);
+			'nome' => 'required|alpha|between:5,40',
+			'rua' => 'required|between:5,35',
+			'bairro' => 'required|between:5,30',
+			'numero' => 'required|numeric',
+			'tipo' => 'required'
+			);
 
-			$validator = Validator::make(Input::all(), $rules);
+		$validator = Validator::make(Input::all(), $rules);
 
-			if ($validator->passes()) {
+		if ($validator->passes()) {
 
-				$unidade->save();
+			$unidade->save();
 
-				return Redirect::action('UnidadesController@index');
+			return Redirect::action('UnidadesController@index');
 
-			} else {
+		} else {
 
-				return Redirect::to('unidades/edit/'.$unidade->id)->withInput()->withErrors($validator);
-			}
+			return Redirect::to('unidades/edit/'.$unidade->id)->withInput()->withErrors($validator);
+		}
 
 	}
 
@@ -128,9 +128,10 @@
 
 		$unidade->delete();
 
-		return Redirect::to('/unidades');
+		return Redirect::action('UnidadesController@index');
 
 	}
 
-		
-		}
+
+}
+
